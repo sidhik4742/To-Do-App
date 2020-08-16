e2f2d928de99027655b9735fe5fde092518c1d4f
sidhik4742 <sidhik64742@gmail.com>
2020-08-16 20:29:12 +0530

    added header and footer and did some styles, added popup screen

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
 import login from "../Login/Login";
 import "./Main.css";
+import ItemEditing from "./ItemEditing";
+import Header from "../Header/Header";
 
 function Main(props) {
   const items = props.items || JSON.parse(sessionStorage.getItem("itemList"));
   const copyItems = JSON.parse(sessionStorage.getItem("itemList"));
   const setItems = props.setItems;
   const [searchItem, setSearchItem] = useState("");
+  const [selectItem, setSelectItem] = useState({});
+  const [showHide, setShowHide] = useState(false);
 
   // console.log(items);
+  // console.log(selectItem);
+  const editItemFun = (event) => {
+    // console.log(event.target);
+    const selectedItemId = parseInt(event.target.dataset.selecteditem);
+    // console.log(selectedItemId);
+    setSelectItem(items[selectedItemId]);
+    setShowHide(true);
+  };
+
   const checkedItemFun = (event) => {
     // console.log(`${event.target.name}" "${event.target.checked}`);
     const btnId = parseInt(event.target.dataset.id);
@@ -52,82 +65,98 @@ function Main(props) {
   };
 
   return (
-    <div className="to-do-main">
-      <main>
-        <div className="search-item">
-          <form>
-            <div>
-              <span>
-                <i className="fa fa-search" aria-hidden="true"></i>
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className="to-do-main">
+        <main>
+          <div className="search-item">
+            <form>
+              <div>
+                <span>
+                  <i className="fa fa-search" aria-hidden="true"></i>
 
-                <input
-                  type="text"
-                  id="add-item"
-                  value={searchItem}
-                  placeholder="Search items"
-                  onChange={changeHandler}
-                />
-              </span>
-            </div>
-          </form>
-        </div>
-        <div className="item-list">
-          <ul>
-            {items.map((item, index) => (
-              <li key={index}>
-                <div className="item-details">
-                  <div className="action">
-                    <div>
-                      <label>{item.itemName}</label>
-                    </div>
-                    {!item.currentStatus? (
+                  <input
+                    type="text"
+                    id="add-item"
+                    value={searchItem}
+                    placeholder="Search items"
+                    onChange={changeHandler}
+                  />
+                </span>
+              </div>
+            </form>
+          </div>
+          <div className="item-list">
+            <ul>
+              {items.map((item, index) => (
+                <li key={index}>
+                  <div className="item-details">
+                    <div className="action">
                       <div>
-                        <input
-                          type="checkbox"
-                          data-id={index}
-                          id={item.itemName}
-                          name={item.itemName}
-                          value={item.itemName}
-                          checked={item.currentStatus}
-                          onChange={checkedItemFun}
-                        />
+                        <label data-selecteditem={index} onClick={editItemFun}>
+                          {item.itemName}
+                        </label>
                       </div>
-                    ) : (
-                      <div className="action-input">
-                        <button
-                          type="button"
-                          data-id={index}
-                          onClick={decrement}
-                        >
-                          {" "}
-                          -{" "}
-                        </button>
-                        <input
-                          type="text"
-                          value={item.quantity}
-                          onChange={changeHandler}
-                          disabled
-                        />
-                        <button
-                          type="button"
-                          data-id={index}
-                          onClick={increment}
-                        >
-                          {" "}
-                          +{" "}
-                        </button>
-                      </div>
-                    )}
+                      {!item.currentStatus ? (
+                        <div className="checkbox">
+                          <input
+                            type="checkbox"
+                            data-id={index}
+                            id={item.itemName}
+                            name={item.itemName}
+                            value={item.itemName}
+                            checked={item.currentStatus}
+                            onChange={checkedItemFun}
+                          />
+                        </div>
+                      ) : (
+                        <div className="action-input">
+                          <button
+                            type="button"
+                            data-id={index}
+                            onClick={decrement}
+                          >
+                            {" "}
+                            -{" "}
+                          </button>
+                          <input
+                            type="text"
+                            value={item.quantity}
+                            onChange={changeHandler}
+                            disabled
+                          />
+                          <button
+                            type="button"
+                            data-id={index}
+                            onClick={increment}
+                          >
+                            {" "}
+                            +{" "}
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <p>Rate/kg: {item.rate}</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>Rate/kg: {item.rate}</p>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </main>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            {showHide && (
+              <ItemEditing
+                selectItem={selectItem}
+                items={items}
+                setShowHide={setShowHide}
+              />
+            )}
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
