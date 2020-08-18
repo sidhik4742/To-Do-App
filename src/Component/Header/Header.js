aa28988ef398b9e93d6dcbca69d1066c3840be70
sidhik4742 <sidhik64742@gmail.com>
2020-08-18 10:11:01 +0530

    adding creat item screen

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
+import ItemCreating from "../Main/ItemCreating";
 
 function Header() {
+  const [showHideCreateItem, setShowHideCreateItem] = useState(false);
   const showMenu = () => {
-      console.log("show menu");
+    console.log("show menu");
   };
   const creatItem = () => {
-      console.log("creat item");
+    console.log("creat item");
+    setShowHideCreateItem(true);
   };
   return (
-    <div className="header">
-      <div className="navicon">
-        <i
-          className="fa fa-bars fa-2x"
-          aria-hidden="true"
-          onClick={showMenu}
-        ></i>
+    <div>
+      <div>
+        {showHideCreateItem && (
+          <ItemCreating setShowHideCreateItem={setShowHideCreateItem} />
+        )}
       </div>
-      <div className="creatItem">
-        <i
-          className="fa fa-plus-circle fa-3x"
-          aria-hidden="true"
-          onClick={creatItem}
-        ></i>
-      </div>
-      <div className="menu">
-        <div className="headerName">
-          <h1>Name</h1>
+      <div className="header">
+        <div className="navicon">
+          <i
+            className="fa fa-bars fa-2x"
+            aria-hidden="true"
+            onClick={showMenu}
+          ></i>
         </div>
-        <div className="menuList">
-          <li>option</li>
-          <li>option</li>
-          <li>option</li>
+        <div className="creatItem">
+          <i
+            className="fa fa-plus-circle fa-3x"
+            aria-hidden="true"
+            onClick={creatItem}
+          ></i>
+        </div>
+        <div className="menu">
+          <div className="headerName">
+            <h1>Name</h1>
+          </div>
+          <div className="menuList">
+            <li>option</li>
+            <li>option</li>
+            <li>option</li>
+          </div>
         </div>
       </div>
     </div>
