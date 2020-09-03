50891fb0770df3d70bff8ee6ef86778caa962bcb
sidhik4742 <sidhik64742@gmail.com>
2020-09-03 20:53:43 +0530

    added back button in save list tab

--- src/Component/Main/SaveList.js
+++ src/Component/Main/SaveList.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./SaveList.css";
 import Axios from "axios";
+import { useHistory } from "react-router-dom";
 
-function SaveList(props) {
+function SaveList() {
+  const history = useHistory();
   const [savedList, setSavedList] = useState([]);
+
+  const classRef = useRef([]);
+  const scaleUpRef = useRef([]);
+  const scaleDownRef = useRef([]);
 
   useEffect(() => {
     Axios({
@@ -19,25 +25,95 @@ function SaveList(props) {
       });
   }, []);
 
-  const tableExpand = ()=>{
-      
-  }
+  const backButtonAction = () => {
+    history.push("/todoapp/main");
+  };
+
+  const tableExpand = (index) => {
+    // console.log(scaleUpRef.current[index]);
+    // console.log(scaleDownRef.current[index]);
+
+    let scaleUp = scaleUpRef.current[index];
+    let scaleDown = scaleDownRef.current[index];
+    let body = classRef.current[index];
+    scaleUp.style.display = "none";
+    scaleDown.style.display = "block";
+    body.style.display = "block";
+    console.log(body);
+  };
+
+  const tableReduce = (index) => {
+    scaleDownRef.current[index].style.display = "none";
+    scaleUpRef.current[index].style.display = "block";
+    let body = classRef.current[index];
+    body.style.display = "none";
+    console.log(body);
+  };
 
   console.log(savedList);
+
   return (
     <div>
       <div className="savedlist">
-        {savedList.map((item, index) => (
+        {savedList.map((list, index) => (
           <div key={index} className="itemDiv">
             <div className="head">
-              <label>Total Rate : {item.TotalRate}</label>
-              <i onClick={tableExpand} className="fa fa-plus-circle fa-2x" aria-hidden="true"></i>
+              <div>
+                <label>
+                  Total Rate : <span>&#x20B9;</span>
+                  {list.TotalRate}
+                </label>
+              </div>
+              <div className="icon">
+                <i
+                  onClick={() => tableExpand(index)}
+                  ref={(el) => (scaleUpRef.current[index] = el)}
+                  className="fa fa-plus-circle fa-2x scaleUp"
+                  aria-hidden="true"
+                ></i>
+                <i
+                  onClick={() => tableReduce(index)}
+                  ref={(el) => (scaleDownRef.current[index] = el)}
+                  className="fa fa-minus-circle fa-2x scaleDown"
+                  aria-hidden="true"
+                ></i>
+              </div>
             </div>
-            <div className="body">
-                <h1>table</h1>
+            <div
+              ref={(el) => (classRef.current[index] = el)}
+              data-id={index}
+              className="showbody"
+            >
+              <table>
+                <thead>
+                  <tr>
+                    <td>Item</td>
+                    <td>Qty</td>
+                    <td>Rate</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {list.SavedList.map((item, id) => (
+                    <tr key={id}>
+                      <td>{item.itemName}</td>
+                      <td>
+                        {item.quantity} {item.type}
+                      </td>
+                      <td>{item.rate}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         ))}
+      </div>
+      <div className="footer_saveList">
+        <i
+          className="fa fa-arrow-circle-left fa-2x"
+          aria-hidden="true"
+          onClick={backButtonAction}
+        ></i>
       </div>
     </div>
   );
